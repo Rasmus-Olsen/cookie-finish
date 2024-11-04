96b2efe1816706628dde88e4356fcad133d383e9
Rasmus Olsen <rasmusolsen@Rasmuss-MacBook-Pro.local>
2024-11-04 12:43:42 +0100

    test

--- src/templates/CookieConsentPopup.jsx
+++ src/templates/CookieConsentPopup.jsx
@@ -9,21 +9,15 @@ const CookieConsentPopup = () => {
   const [acceptCookies, setAcceptCookies] = useState(false);
 
   return (
-    <section
-      className={`${
-        acceptCookies && "hidden"
-      } flex flex-col justify-between p-6 text-white bg-black w-72 h-80 rounded-3xl`}
-    >
+    <section className={`${acceptCookies && "hidden"} flex flex-col justify-between p-6 text-white bg-black w-72 h-80 rounded-3xl`}>
       <header className="flex justify-between">
         <LiaCookieBiteSolid size={50} />
         <IoIosClose className="cursor-pointer" size={25} />
       </header>
       <footer className="flex flex-col">
         <CookieConsentText />
-        <CookieConsentButton
-          test="Test string prop"
-          action={setAcceptCookies}
-        />
+        <p>lille test</p>
+        <CookieConsentButton test="Test string prop" action={setAcceptCookies} />
       </footer>
     </section>
   );
